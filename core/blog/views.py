79eeeea5a3055510a61d6634275b31108c06e560
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-06-11 07:57:46 +0330

    3-14- معرفی List View

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from .models import Post
 # Create your views here.
 def indexView (request):
     return render(request, "index.html")
+
+
+class IndexView(TemplateView):
+    template_name= 'index.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = "ali"
+        return context
+
+class PostList(ListView):
+    context_object_name = "posts"
+    def get_queryset(self):
+        posts = Post.objects.filter(status= True)
+        return posts
+
