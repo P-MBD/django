c31944dc58baace4812660e37dfbf23a172c56fb
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-06-15 10:21:03 +0330

    بررسی عملکرد در Api_View

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "api-v1"
 urlpatterns = [
     path('post/',views.postList, name="post-list"),  
+    path('post/<int:id>/',views.postDetail, name="post-detail"),  
    ]
