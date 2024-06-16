f1a549e4105aa80b540a7c5bf39ec58d3a1cea22
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-06-16 07:12:45 +0330

    قسمت اول دریافت داده‌های ورودی از POST و Validation

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -6,10 +6,12 @@ from django.shortcuts import get_object_or_404
 
 @api_view(["GET","POST"])
 def postList(request):
-    posts =Post.objects.filter(status=True)
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
-
+    if request.method == "GET":
+        posts =Post.objects.filter(status=True)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        return Response("ok")
 @api_view()
 def postDetail(request,id):
     post = get_object_or_404(Post,pk=id)
