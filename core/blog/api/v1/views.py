600c2280796853e38129d842002ba246adb06357
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-06-16 11:31:55 +0330

    5-8- به روز رسانی اطلاعات پست از طریق Put

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -17,10 +17,17 @@ def postList(request):
         else:
             return Response(serializer.errors)
 
-@api_view()
+@api_view(["GET","PUT"])
 def postDetail(request,id):
     post = get_object_or_404(Post,pk=id)
-    print(post.__dict__)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
+    if request.method == "GET":
+         serializer = PostSerializer(post)
+         return Response(serializer.data)
+    elif request.method =="PUT":
+        serializer = PostSerializer(post, data= request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
 
