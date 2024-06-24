051bf150ff31581143e0c1746fb847e89dd045ae
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-06-24 06:59:07 +0330

    6-14- توابع وابسته به درخواست های درونی serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,14 +1,12 @@
 from rest_framework import serializers
 from ...models import Post,Category
-
 # class PostSerializer(serializers.Serializer):
 #     title = serializers.CharField(max_length=255)
-
 
 class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source="get_snippet")
     relative_path = serializers.URLField(source="get_absolute_api_url", read_only=True)
-
+    absolute_url = serializers.SerializerMethodField(method_name='get_abs_url')
     class Meta:
         model = Post
         fields = [
@@ -20,10 +18,15 @@ class PostSerializer(serializers.ModelSerializer):
             "snippet",
             "category",
             "status",
+            "absolute_url",
             "relative_path",
             "created_date",
             "published_date",
         ]
+    def get_abs_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.pk)
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
