b52ed91c88f37ee9b30ad2bf46fb5daa30b7334b
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-06-27 11:36:20 +0330

    FIlterBackend در Restframework

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -9,76 +9,21 @@ from rest_framework.generics import GenericAPIView,ListCreateAPIView, RetrieveUp
 from rest_framework import mixins, viewsets
 from rest_framework.decorators import api_view, permission_classes, action
 from .permissions import IsOwnerOrReadOnly
-"""@api_view(["GET","POST"])
-@permission_classes([IsAdminUser])
-    def postList(request):
-    if request.method == "GET":
-        posts =Post.objects.filter(status=True)
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = PostSerializer(data= request.data)
-        if serializer.is_valid():
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)"""
+from django_filters.rest_framework import DjangoFilterBackend
 
-'''class PostList(ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = PostSerializer
-    queryset = Post.objects.filter(status=True)
- '''
+
 class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    @action(methods = ["get"], detail=False)
-    def get_ok(self, request):
-        return Response({'detail','ok'})
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'author', 'status']
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes= [IsAuthenticated]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
-"""@api_view(["GET","PUT","DELETE"])
-def postDetail(request,id):
-    post = get_object_or_404(Post,pk=id)
-    if request.method == "GET":
-         serializer = PostSerializer(post)
-         return Response(serializer.data)
-    elif request.method =="PUT":
-        serializer = PostSerializer(post, data= request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method =="DELETE":
-        post.delete()
-        return Response({"detail":"item removed successfully"},status=status.HTTP_204_NO_CONTENT)"""
-
-'''class PostDetail(APIView):
-    """getting detail of the post and edit plus removing it"""
-    permission_classes =[IsAuthenticatedOrReadOnly]
-    serializer_class = PostSerializer
-    def get(self, request,id):
-        """getting detail of the post and  edit plus removing it"""
-        post=get_object_or_404(Post, pk=id)
-        serializer = self.serializer_class(post)
-        return Response(serializer.data)
-    def put(self, request, id):
-        """editing the post data"""
-        post = get_object_or_404(Post, pk=id)
-        serializer = self.serializer_class(post)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    def delete(self, request, id):
-        post = get_object_or_404(Post, pk=id)
-        post.delete()
-        return Response({"detail":"item removed successfully"},status=status.HTTP_204_NO_CONTENT)'''
 
 
-class PostDetail(RetrieveUpdateDestroyAPIView):
-    permission_classes =[IsAuthenticatedOrReadOnly] 
-    serializer_class = PostSerializer
-    queryset = Post.objects.filter(status=True)
+
 
