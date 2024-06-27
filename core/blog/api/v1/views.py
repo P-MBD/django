d7be62c6734b29900a1115c2f1d4d1ba3300a348
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-06-27 11:47:59 +0330

    SearchFilter در Restframework

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -10,14 +10,15 @@ from rest_framework import mixins, viewsets
 from rest_framework.decorators import api_view, permission_classes, action
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.filters import SearchFilter
 
 class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_fields = ['category', 'author', 'status']
+    search_fields = ['title', 'content']
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes= [IsAuthenticated]
