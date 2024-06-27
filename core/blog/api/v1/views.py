86e2d9bbcddd953bdb707b5d8737f0f6b15a12a3
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-06-27 11:53:21 +0330

    OrderingFilter یا Sort در Restframework

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -10,15 +10,16 @@ from rest_framework import mixins, viewsets
 from rest_framework.decorators import api_view, permission_classes, action
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter,OrderingFilter]
     filterset_fields = ['category', 'author', 'status']
     search_fields = ['title', 'content']
+    ordering_fields = ['published_date']
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes= [IsAuthenticated]
