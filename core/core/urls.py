b2c819d80e7d511c43a9ae364055d25654e7fb78
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-07-01 10:02:42 +0330

    گرفتن خروجی از swagger برای postman

--- core/core/urls.py
+++ core/core/urls.py
@@ -23,9 +23,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Blog API",
       default_version='v1',
-      description="Test description",
+      description="This is a Test api for Maktabkhooneh  project",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
@@ -40,6 +40,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path("blog/", include("blog.urls")),
+    path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
