67d5eeb51f94f51a6769e21b25a6c643033a2f0a
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-07-03 07:48:39 +0330

    پیاده‌سازی Token Authentication (login)

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from . import views
+from rest_framework.authtoken.views import ObtainAuthToken
 app_name = 'api-v1'
 urlpatterns = [
     #registrtion
-    path('registration/', views.RegistrationApiView.as_view(), name='registration')
+    path('registration/', views.RegistrationApiView.as_view(), name='registration'),
+    path('token/login', ObtainAuthToken.as_view(), name='token-login')
     #change password
     #reset password
     #login token 
