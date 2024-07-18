32b578ddeeb3aa575d506dff64e9277d9b3db6e2
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-07-18 08:24:18 +0330

    8-23- Making tokens manually

--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -12,6 +12,7 @@ from django.shortcuts import get_object_or_404
 from ..utils import EmailThread
 from mail_templated import EmailMessage
 from ..utils import EmailThread
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
@@ -92,9 +93,21 @@ class ProfileApiView(generics.RetrieveUpdateAPIView):
    
 class TestEmailSend(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
-        message = EmailMessage('email/hello.tpl', {'name': 'ali'}, 'admin@gmail.com', ['bigdeli.ali@gmail.com'])
-        message.send()
-        return Response("email send")
+        self.email = "test@gmail.com"
+        user_obj = get_object_or_404(User, email=self.email)
+        token = self.get_tokens_for_user(user_obj)
+        email_obj = EmailMessage(
+            "email/hello.tpl",
+            {"token": token},
+            "admin@admin.com",
+            to=[self.email],)
+        EmailThread(email_obj).start()
+        return Response("email sent")
+
+    def get_tokens_for_user(self, user):
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
+
 
 
         
--- core/templates/email/hello.tpl
+++ core/templates/email/hello.tpl
@@ -1,10 +1,9 @@
 {% extends "mail_templated/base.tpl" %}
 
 {% block subject %}
-Hello {{ name }}
+Account Activations 
 {% endblock %}
 
 {% block html %}
-This is an <strong>html</strong> message.
-<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ_AxHyJTfxcxpt0bn313ReGPqfSCNkqEhuQ&usqp=CAU'>
+{{token}}
 {% endblock %}
